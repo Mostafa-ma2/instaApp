322c76488415a024a982eb86c6da3b15a505570b
Mostafa Abedini <mostafaabedinima2@gmail.com>
2020-05-24 19:31:13 +0430

    Add Profile

---
+++ js/Profile.js
@@ -0,0 +1,34 @@
+var body = document.getElementById("body");
+// Serch box
+var InputSearch = document.getElementById("input-search");
+var Serch = document.getElementById("search");
+var txtSearchText = document.querySelector('.text-search');
+var IconSearch = document.getElementById("search-icon");
+var CloseSearch = document.getElementById("close-Search");
+InputSearch.addEventListener("click", SearchBox)
+
+function SearchBox() {
+    CloseSearch.style.display = "inline-block";
+}
+// Serch hide and show
+InputSearch.addEventListener("keyup", function() {
+    TserchNone(this)
+})
+
+function TserchNone(eh) {
+    if (eh.value == "") {
+        txtSearchText.style.Left = "57px;";
+        txtSearchText.style.display = "inline-block";
+    } else {
+        txtSearchText.style.display = "none";
+
+    }
+}
+//close serch
+CloseSearch.addEventListener("click", closeSerch)
+
+function closeSerch(eh) {
+    InputSearch.value = "";
+    CloseSearch.style.display = "none";
+    txtSearchText.style.display = "inline-block";
+}
